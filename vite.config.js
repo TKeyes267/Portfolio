577a8eae35e180e625c38ac165fb88844095332b
TKeyes267 <tomkeyes26@gmail.com>
2023-10-12 21:40:34 +0100

    fix Browser Router bug

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
-    base: "/Portfolio/",
+    base: "https://TKeyes267.github.io/Portfolio/",
   };
 
   if (command !== "serve") {
