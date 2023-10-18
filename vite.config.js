c7dad7811eac0b194b892c8816da5b75463e09a4
TKeyes267 <tomkeyes26@gmail.com>
2023-10-18 16:30:38 +0100

    change config path

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
-    base: "/Portfolio/",
+    base: "/",
   };
 
   if (command !== "serve") {
-    config.base = "/Portfolio/";
+    config.base = "/";
   }
 
   return config;
